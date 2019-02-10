36249c8e5ee86cd4985260c12a843f576706f8b9
nobarudo <esukab@gmail.com>
2019-02-11 02:40:52 +0900

    add userName

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
   "log"
+  "net/http"
   "github.com/gin-gonic/gin"
   "html/template"
   "gopkg.in/olahol/melody.v1"
+  //"github.com/gin-gonic/contrib/renders/multitemplate"
 )
 
 func main() {
@@ -16,6 +18,22 @@ func main() {
     html := template.Must(template.ParseFiles("templates/base.tmpl", "templates/index.tmpl"))
     router.SetHTMLTemplate(html)
     c.HTML(200, "base.tmpl", gin.H{})
+  })
+
+  router.POST("/chat", func(c *gin.Context) {
+    test := c.PostForm("userName")
+    log.Printf("debug:", test)
+    html := template.Must(template.ParseFiles("templates/base.tmpl", "templates/chat.tmpl"))
+    router.SetHTMLTemplate(html)
+    //templates := multitemplate.New()
+    //templates.AddFromFiles("contact", "templates/base.tmpl", "templates/chat.tmpl")
+    c.HTML(200, "chat.tmpl", gin.H{
+      "userName": test,
+    })
+  })
+
+  router.GET("/chat", func(c *gin.Context) {
+    c.Redirect(http.StatusMovedPermanently,"/")
   })
 
   router.GET("/ws", func(c *gin.Context) {
