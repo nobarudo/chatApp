1874db5aee94e0ddca244619ad7be64b43372289
nobarudo <esukab@gmail.com>
2019-02-13 02:06:07 +0900

    add session

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
   "html/template"
   "github.com/gin-gonic/gin"
   "gopkg.in/olahol/melody.v1"
+  "github.com/gin-gonic/contrib/sessions"
   //"github.com/gin-gonic/contrib/renders/multitemplate"
 )
 
@@ -14,6 +15,8 @@ func main() {
   router.Static("/assets", "./assets")
   router.LoadHTMLGlob("templates/*.tmpl")
   m := melody.New()
+  store := sessions.NewCookieStore([]byte("secret"))
+  router.Use (sessions.Sessions("session", store))
 
   router.GET("/", func(c *gin.Context) {
     html := template.Must(template.ParseFiles("templates/base.tmpl", "templates/index.tmpl"))
@@ -25,6 +28,11 @@ func main() {
     name := c.PostForm("userName")
     html := template.Must(template.ParseFiles("templates/base.tmpl", "templates/chat.tmpl"))
     router.SetHTMLTemplate(html)
+
+    session := sessions.Default(c)
+    session.Set("name", name)
+    session.Save()
+
     log.Println("新しいユーザが参加しました. userName:", name)
     systemMsg := []byte("[システム] > "+name+"が参加しました. ")
     m.Broadcast(systemMsg)
@@ -40,7 +48,13 @@ func main() {
   })
 
   router.GET("/ws", func(c *gin.Context) {
+    session := sessions.Default(c)
+    name := session.Get("name")
+    temp := name.(string)
+    systemMsg := []byte("[システム] > "+temp+"が退出しました. ")
     m.HandleRequest(c.Writer, c.Request)
+    log.Println("debug:", name)
+    m.Broadcast(systemMsg)
   })
 
   m.HandleMessage(func(s *melody.Session, msg []byte) {
